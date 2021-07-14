b645234747355693a127e3da4dde04f03a078e4e
Doug VonMoser <doug.vonmoser@gmail.com>
2021-07-14 17:20:55 -0500

    hopefully increasing ttl on guardian tokens

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,8 @@ config :material, repo: Funny.Repo
 
 config :funny, Funny.Accounts.Guardian,
   issuer: "funny",
-  secret_key: "g++gf/rmFG+z9cc3PXa8dPfJ001v2zU3skXLu5yr2YHNFF5RXWcZgsIKYzT72Jvp"
+  secret_key: "g++gf/rmFG+z9cc3PXa8dPfJ001v2zU3skXLu5yr2YHNFF5RXWcZgsIKYzT72Jvp",
+  ttl: {52, :weeks}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
