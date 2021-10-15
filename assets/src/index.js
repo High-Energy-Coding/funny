6c2f175b4b31d3111550b25c26aae653f8a538f8
Doug VonMoser <doug.vonmoser@gmail.com>
2021-10-15 10:16:32 -0500

    trigger login loading feedback

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,3 +4,8 @@ import * as serviceWorker from './serviceWorker'
 import 'phoenix_html';
 
 serviceWorker.register();
+
+window.trigger_login_loading = () => {
+    document.querySelector(".spinny").classList.add("yes-spinny")
+    document.querySelector(".spinner-center").classList.add("yes-gray")
+}
